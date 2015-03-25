5be208a02a2215b2033cc831434dbd22398193bb
TwanVermeulen <twan.vermeulen@skydreams.com>
2015-03-25 11:35:12 +0100

    Truncate wp_domain_mapping table during db update

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -1,10 +1,15 @@
 echo "Import latest live db backup"
 gzip -dc /vagrant/wordpress.sql.gz | mysql --user=root --password=root wordpress-skydreams
 
+echo "Truncate domain mapping table"
+mysql --user=skydev --password=skydev wordpress-skydreams -e 'TRUNCATE wp_domain_mapping;'
+
+echo "Replace live domain with dev domain"
 php /vagrant/scripts/search-replace-db/srdb.cli.php -h localhost -u skydev -n wordpress-skydreams -p skydev -s https://wordpress.skydreams.com -r http://wordpress.skydreams.com
 php /vagrant/scripts/search-replace-db/srdb.cli.php -h localhost -u skydev -n wordpress-skydreams -p skydev -s wordpress.skydreams.com -r wordpress.skydreams.com.dev
 
-echo "Add skydev user"
 cd /vagrant/www/wordpress-skydreams/htdocs/
+
+echo "Add skydev user"
 wp user create skydev test@skydreams.com --user_pass=skydev --role=administrator
 wp super-admin add test@skydreams.com
