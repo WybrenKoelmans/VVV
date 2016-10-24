83132feda8ab7e24c2e4236172fab0fefa828ef3
Twan Vermeulen <twan.vermeulen@skydreams.nl>
2016-10-24 12:01:46 +0200

    Updated dashboard

--- www/default/dashboard-custom.php
+++ www/default/dashboard-custom.php
@@ -1,9 +1,1 @@
-<?php
-
-
-function redirect_to_vvv_dash( $url, $status_code = 301 ) {
-	header( 'Location: ' . $url, true, $status_code );
-	die();
-}
-
-redirect_to_vvv_dash( '/dashboard/index.php', 302 );
+<?php require __DIR__ . '/material-dashboard/index.php';
