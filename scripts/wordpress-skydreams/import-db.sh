9bbb1e48194e1601074cf55b4ef0a3c88fd041ab
Twan Vermeulen <twan.vermeulen@skydreams.nl>
2018-01-10 12:59:59 +0100

    Separated the build dev db functionality from the import. Import is much quicker now!

--- scripts/wordpress-skydreams/import-db.sh
+++ scripts/wordpress-skydreams/import-db.sh
@@ -1,52 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -f /vagrant/wordpress.sql.gz ]; then
-    echo "wordpress.sql.gz can't be found. Please add this file (see docs) and provision again!"
+if [ ! -f /vagrant/wordpress-dev.sql.gz ]; then
+    echo "wordpress-dev.sql.gz can't be found. Please add this file to the root directory and run this again!"
     exit 0
 fi
 
-echo "Importing latest live db backup..."
-gzip -dc /vagrant/wordpress.sql.gz | mysql --user=root --password=root wordpress-skydreams
-
-echo "Start db string replacement #1: replace https://wordpress.skydreams.com -> http://wordpress.skydreams.com"
-php /vagrant/scripts/search-replace-db/srdb.cli.php -h localhost -u skydev -n wordpress-skydreams -p skydev -s https://wordpress.skydreams.com -r http://wordpress.skydreams.com -v false
-
-echo "Start db string replacement #2: replace wordpress.skydreams.com -> wordpress.skydreams.com.dev"
-php /vagrant/scripts/search-replace-db/srdb.cli.php -h localhost -u skydev -n wordpress-skydreams -p skydev -s wordpress.skydreams.com -r wordpress.skydreams.com.dev -v false
-
-cd /vagrant/www/wordpress-skydreams/htdocs
-
-prefix="http://www."
-suffix="/"
-
-wp plugin deactivate redirection  --network
-
-# Update site specific urls
-for url in $(wp site list --allow-root --field=url)
-
-do
-    # Update disqus_form_url as we don't want to affect comments @ production
-    wp option update disqus_forum_url skydreams-test --url=$url --allow-root
-
-    # Add skydev user to each site
-    wp user set-role skydev administrator --url=$url --allow-root
-
-    if [[ "$url" =~ $mappedDomainDetection ]]; then
-        host=${url#$prefix}
-        host=${host%$suffix}
-        wwwurl="www.$host"
-        liveurl="https://www.$host"
-        devUrl="$host.dev"
-        fullDevUrl="http://$devUrl"
-
-        wp search-replace $wwwurl $devUrl wp_blogs
-        wp search-replace $liveurl $fullDevUrl --network
-    fi
-done
-
-wp plugin activate redirection  --network
-
-echo 'Add skydev user'
-wp user create skydev test@skydreams.com --user_pass=skydev --role=administrator --allow-root
-wp super-admin add test@skydreams.com --allow-root
-wp core update-db --allow-root --network
+echo "Importing dev db..."
+gzip -dc /vagrant/wordpress-dev.sql.gz | mysql --user=root --password=root wordpress-skydreams
