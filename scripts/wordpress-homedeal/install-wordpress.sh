03bd01c173c77593566642530bd02e721c34ed53
Jetse Jacobi <jetse.jacobi@skydreams.com>
2018-08-03 09:55:45 +0200

    #38709 - [Header] Dropdown projects/categories (#22)

--- scripts/wordpress-homedeal/install-wordpress.sh
+++ scripts/wordpress-homedeal/install-wordpress.sh
@@ -4,10 +4,12 @@ cd /vagrant/www/wordpress-homedeal/htdocs
 
 wp core download --allow-root
 wp core config --dbname="wordpress-homedeal" --dbuser=skydev --dbpass=skydev --dbhost="localhost" --allow-root --extra-php <<PHP
+define('APP_ENV', 'development');
 define('WP_DEBUG', true );
 define('WP_DEBUG_LOG', true );
 define('PARTNER_URL', 'http://partners.skydreams.com.dev.skydreams.com');
 define('SKYAPI_URL', 'http://skyapi.net.dev.skydreams.com');
+define('SKYCDN_URL', 'http://resources.skycdn.net.dev.skydreams.com');
 define('ALLOW_UNFILTERED_UPLOADS', true );
 
 define('WP_ALLOW_MULTISITE', true );
